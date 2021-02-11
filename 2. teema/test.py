67d859d35153eeef9cb3a8f5d3797eaddea5023f
Marko Arula <marko.arula@khk.ee>
2021-02-11 15:16:20 +0200

    Loodud klass ja tema eksemplaar koos testimisega

--- 2. teema/test.py
+++ 2. teema/test.py
@@ -1,16 +1,16 @@
 from Restoraan import Restoraan
-# loome 1 eksemplaari Restoraan klassi põhjal
+# loome eksemplaarid Restoraan klassi põhjal
 restoraan1 = Restoraan()
 restoraan2 = Restoraan()
 restoraan3 = Restoraan()
-# määrame eksemplaari atribuutidele konkreetsed väärtused
+# määrame eksemplaaride atribuutidele konkreetsed väärtused
 restoraan1.restoraani_nimi = "McDonalds"
 restoraan1.soogi_tyyp = "burgerid ja friikartulid"
 restoraan2.restoraani_nimi = "Hesburger"
 restoraan2.soogi_tyyp = "burgerid"
 restoraan3.restoraani_nimi = "KFC"
 restoraan3.soogi_tyyp = "kana"
-# kutsume eksemplaari meetodid tööle
+# kutsume eksemplaaride meetodid tööle
 restoraan1.restoraani_kirjeldus()
 restoraan1.ava_restoraan()
 restoraan2.restoraani_kirjeldus()
