fb330448cd494423c1770d30660bb3ae89153026
Marko Arula <marko.arula@khk.ee>
2021-02-11 13:45:49 +0200

    Klassi ja eksemplaari loomise katse

---
+++ 2. teema/Matemaatika.py
@@ -0,0 +1,5 @@
+class Matemaatika():
+    number = 5
+    pass
+    def liitmine(self, arv):
+        print(self.number + arv)
---
+++ 2. teema/test.py
@@ -0,0 +1,7 @@
+from Matemaatika import Matemaatika
+# from file import klass
+a = Matemaatika()
+print(a.number)
+a.number = 8
+print(a.number)
+a.liitmine(4)
