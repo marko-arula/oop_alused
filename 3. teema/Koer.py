a13dff24e031367160d2349a00825e9d6b55d2f4
Marko Arula <marko.arula@khk.ee>
2021-02-12 12:34:19 +0200

    Klassi konstruktor kasutab vaikimisi väärtused

--- 3. teema/Koer.py
+++ 3. teema/Koer.py
@@ -5,6 +5,10 @@ class Koer():
         self.hyydnimi = h
         self.vanus = v
         self.saba = True
+    # dekonstruktor
+    def __del__(self):
+
+        print("Kahjuks koer juba nii vana, et läheb ära")
     # meetod
     def kirjeldus(self):
         print("Koer nimega " + self.hyydnimi + " on " + str(self.vanus) + " aastat vana")
--- 3. teema/test.py
+++ 3. teema/test.py
@@ -6,3 +6,13 @@ puppy = Koer("Puppy")
 puppy.kirjeldus()
 if(puppy.saba):
     print("Puppy liputab")
+
+vanus = 5
+while(vanus < 15):
+
+    puppy.vanus = vanus
+    puppy.kirjeldus()
+    if (puppy.vanus == 15):
+        del puppy
+    vanus += 5
+input()
