f7cd5fbee5f7e06254ade9f76efdf7c7f5696164
Marko Arula <marko.arula@khk.ee>
2021-02-12 11:31:15 +0200

    Klassi konstruktor kasutab vaikimisi väärtused

--- 3. teema/Koer.py
+++ 3. teema/Koer.py
@@ -1,11 +1,10 @@
 class Koer():
-    hyydnimi = ""
-    vanus = 0
+
     # konstruktor
-    def __init__(self, h, v):
+    def __init__(self, h, v = 0):
         self.hyydnimi = h
         self.vanus = v
-
+        self.saba = True
     # meetod
     def kirjeldus(self):
         print("Koer nimega " + self.hyydnimi + " on " + str(self.vanus) + " aastat vana")
--- 3. teema/test.py
+++ 3. teema/test.py
@@ -1,3 +1,8 @@
 from Koer import Koer
 nublu = Koer("Nublu", 5)
 nublu.kirjeldus()
+
+puppy = Koer("Puppy")
+puppy.kirjeldus()
+if(puppy.saba):
+    print("Puppy liputab")
