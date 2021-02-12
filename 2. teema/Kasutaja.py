d22e7893023c0e4d9b441e9c2c32ab263a1fff0d
Marko Arula <marko.arula@khk.ee>
2021-02-12 10:34:08 +0200

    Kasutaja ülesanne atribuudid

--- 2. teema/Kasutaja.py
+++ 2. teema/Kasutaja.py
@@ -4,6 +4,9 @@ class Kasutaja():
     kasutaja_nimi = ""
     parool = ""
     def kirjelda_kasutaja(self):
-        print("Kasutaja eesnimi on " + (self.eesnimi) + ", perenimi on " + self.perenimi + ", kasutajanimi on " + self.kasutaja_nimi + " ja parool on " + self.parool + ".")
+        print("Kasutaja eesnimi on " + (self.eesnimi))
+        print("Perenimi on " + self.perenimi)
+        print("Kasutajanimi on " + self.kasutaja_nimi)
+        print("Parool on " + self.parool + ".")
     def tervita_kasutaja(self):
         print("Tere tulemast " + self.eesnimi + self.perenimi)
