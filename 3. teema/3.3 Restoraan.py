7bddea87bc06a2f80fe88fd1669f77fb3813ba00
Marko Arula <marko.arula@khk.ee>
2021-02-16 10:24:15 +0200

    Ülesanne 9 Moodle

---
+++ 3. teema/3.3 Restoraan.py
@@ -0,0 +1,10 @@
+from Restoraan import Restoraan
+
+restoraan1 = Restoraan("McDonalds", "burgerid", 12)
+
+restoraan1.restoraani_kirjeldus()
+restoraan1.ava_restoraan()
+
+restoraan1.maara_teenindatud_kylalised(20)
+restoraan1.suurenda_teenindatud_kylalised(15)
+
--- 3. teema/Restoraan.py
+++ 3. teema/Restoraan.py
@@ -1,10 +1,21 @@
 class Restoraan():
 
-    def __init__(self, nimi, tyyp):
+    def __init__(self, nimi, tyyp, teenindatud_kylastajad):
         self.restoraani_nimi = nimi
         self.soogi_tyyp = tyyp
+        self.teenindatud_kylastajad = teenindatud_kylastajad
     def restoraani_kirjeldus(self):
         print("Restoraani nimi on " + str(self.restoraani_nimi) + ", söögi tüüp on " + str(self.soogi_tyyp))
 
     def ava_restoraan(self):
         print("Restoraan on avatud")
+
+    def maara_teenindatud_kylalised(self, tk):
+        print("Ennem oli teenindatud ", self.teenindatud_kylastajad)
+        self.teenindatud_kylastajad = tk
+        print("külaliste arv on määratud nüüdseks ", tk)
+
+    def suurenda_teenindatud_kylalised(self, tk):
+        print("Teenindatud külaliste arv oli ennem ", self.teenindatud_kylastajad)
+        self.teenindatud_kylastajad = self.teenindatud_kylastajad + tk
+        print("Teenindatud külaliste arv on nüüd ", self.teenindatud_kylastajad)
