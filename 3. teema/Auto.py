0fb4052f07907af5ee7957dc094da51fbc13df7a
Marko Arula <marko.arula@khk.ee>
2021-02-16 09:25:38 +0200

    Kordamine - klassi atribuutide väärtuse määramise võimalused - otse ja meetodite abil

--- 3. teema/Auto.py
+++ 3. teema/Auto.py
@@ -13,3 +13,12 @@ class Auto():
 
     def odomeeter(self):
         print("Antud auto on sõitnud läbi " + str(self.odomeetri_nait) + " km.")
+    # 2 - atribuuti väärtuse muutmine meetodi abil
+    def uuenda_odomeeter(self, km):
+        if km >= self.odomeetri_nait:
+            self.odomeetri_nait = km
+        else:
+            print("Ei ole võimalik tagasi keerata odomeetri näitu")
+
+    def suurenda_odomeeter(self, km):
+        self.odomeetri_nait += km
--- 3. teema/test.py
+++ 3. teema/test.py
@@ -5,6 +5,12 @@ minu_uus_auto = Auto("Skoda", "Octavia", 2020)
 
 print(ainari_uus_auto.kirjeldus())
 ainari_uus_auto.odomeeter()
+# 1 - objekti atribuuti otse väärtustamine
+# ainari_uus_auto.odomeetri_nait = 2
+ainari_uus_auto.uuenda_odomeeter(2)
+ainari_uus_auto.odomeeter()
+ainari_uus_auto.suurenda_odomeeter(30)
+ainari_uus_auto.odomeeter()
 
 
 print(minu_uus_auto.kirjeldus())
