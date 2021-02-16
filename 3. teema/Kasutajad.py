c32a10ece156eac5341a62e1960ddddda5ca62ad
Marko Arula <marko.arula@khk.ee>
2021-02-16 10:40:05 +0200

    Ülesanne 10 Moodle

--- 3. teema/Kasutajad.py
+++ 3. teema/Kasutajad.py
@@ -1,13 +1,25 @@
 class Kasutajad():
     kasutajanimi = ""
     roll = "tavakasutaja"
-    def __init__(self, e, p):
+    def __init__(self, e, p, ssl):
         self.eesnimi = e
         self.perenimi = p
+        self.sisselogimiskatsed = ssl
     def kirjelda_kasutaja(self):
         print("Kasutaja eesnimi on " + (self.eesnimi))
         print("Perenimi on " + self.perenimi)
         print("Kasutajanimi on " + self.kasutajanimi)
         print("Kasutaja on " + self.roll)
+    def tervita_kasutaja(self):
+        print("Tere! " + str(self.eesnimi) + " " + str(self.perenimi) + ".")
+
     def maara_kasutaja_nimi(self, k):
         self.kasutajanimi = k
+
+    def suurenda_sisselogimiskatsed(self):
+        self.sisselogimiskatsed = self.sisselogimiskatsed + 1
+        print("Antud kasutajal on ", self.sisselogimiskatsed, " sisselogimise katset")
+
+    def reset_sisselogimiskatsed(self):
+        self.sisselogimiskatsed = 0
+        print("Kasutaja sisselogimisekatsed on taastatud")
---
+++ 3. teema/Ülesanne 3.4 Kasutajad.py
@@ -0,0 +1,11 @@
+from Kasutajad import Kasutajad
+
+kasutaja1 = Kasutajad("Mari", "Puu", 0)
+kasutaja1.maara_kasutaja_nimi("Marikene")
+
+
+kasutaja1.kirjelda_kasutaja()
+
+kasutaja1.suurenda_sisselogimiskatsed()
+
+kasutaja1.reset_sisselogimiskatsed()
