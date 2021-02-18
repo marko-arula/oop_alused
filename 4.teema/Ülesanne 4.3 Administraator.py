1b9c2ea48fa0bde12e63c57d5d1da8d80ef83170
Marko Arula <marko.arula@khk.ee>
2021-02-18 14:46:03 +0200

    Ülesanne 4.3 Administraator

---
+++ 4.teema/Ülesanne 4.3 Administraator.py
@@ -0,0 +1,8 @@
+from Kasutajad import *
+
+
+Admin1 = Admin("Admin", "Root", "Administraator")
+
+Admin1.kirjelda_kasutaja()
+Admin1.privileegid("Lubatud lisada kasutajad, lubatud lisada kasutajad, lubatud eemaldada kasutajad")
+Admin1.naita_privileegid()
