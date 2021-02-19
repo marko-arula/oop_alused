10bd743f0936cb1ef25fc28a85ac477eabc4aa1a
Marko Arula <marko.arula@khk.ee>
2021-02-19 12:33:05 +0200

    Ülesanne 5.1 Ristkülik

--- 5.teema/Ülesanne 5.1 Ristkülik.py
+++ 5.teema/Ülesanne 5.1 Ristkülik.py
@@ -1,29 +1,35 @@
-class Ristkülik():
-    def __init__(self, korgus, laius, symbol):
-        self.laius = int(laius)
-        self.korgus = int(korgus)
-        self.symbol = str(symbol)
+class Ristkylik():
+    def __init__(self, l, k, s):
+        self.laius = l
+        self.korgus = k
+        self.symbol = s
 
     def __str__(self):
         ruut = []
         for i in range(self.korgus):
-            ruut.append(self.symbol * self.laius)
-        ruut = '\n'.join(ruut)
-        return ruut
+            symbolid = self.symbol * self.laius
+            ruut.append(symbolid)
+        ristkylik_tekstina = "\n".join(ruut)
+        return ristkylik_tekstina
 
-    def __add__(self, x):
-        self.laius = self.laius + x.laius
-        self.korgus = self.korgus + x.korgus
-        self.symbol = self.symbol + x.symbol
+    def __add__(self, teine_ristkylik):
+        uus_laius = self.laius + teine_ristkylik.laius
+        uus_korgus = self.korgus + teine_ristkylik.korgus
+        uus_symbol = self.symbol
+        return Ristkylik(uus_laius, uus_korgus, uus_symbol)
 
 
-kujund1 = Ristkülik(10, 3, '*')
+
+kujund1 = Ristkylik(4, 2, "*")
 print(kujund1)
-kujund2 = Ristkülik(8, 3, 'z')
+
+kujund2 = Ristkylik(8, 3, 'o')
 print(kujund2)
 
-kujund1.__add__(kujund2)
+uus_kujund = kujund1 + kujund2
+print(uus_kujund)
 
-print(kujund1)
+
+
 
 # Marko
