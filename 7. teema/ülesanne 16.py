60e79db62ace8c617c758d583bd16f1b4a97cf8f
Marko Arula <marko.arula@khk.ee>
2021-02-25 15:59:19 +0200

    Ülesanne 16 - Administraator ja privileegid

---
+++ 7. teema/ülesanne 16.py
@@ -0,0 +1,8 @@
+from Kasutajad import *
+
+Kasutaja1 = Kasutajad("Tanel", "Peep", "Kasutaja")
+
+admin = Admin("Jüri", "Paavel", "Admin")
+
+admin.kirjelda_kasutaja()
+admin.privileegid.naita_privileegid()
